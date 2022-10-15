050bd4b7147dacb2b7fb423c0c4863422b5cb2de
Salikhovakbar <pcfjkjhkk@gmail.com>
2022-10-15 09:39:55 +0500

    second push

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,22 @@ boxEl.classList.remove("second-style")
 openBtn.addEventListener('click' , () => {
     boxEl.classList.add("second-style")  
 })
+const boxElTwo = document.querySelector(".light-circle-box")
+
+const mainLightBox = document.querySelector(".light-box");
+boxElTwo.addEventListener('click', () => {
+    
+    if(document.body.style.backgroundColor === "white"){
+        document.body.style.backgroundColor = "black"
+    }
+    else{
+        document.body.style.backgroundColor = "white"
+    }
+    if(mainLightBox.style.backgroundColor === "white"){
+        mainLightBox.style.backgroundColor = "green"
+    }
+    else{
+        mainLightBox.style.backgroundColor = "white"
+    }
+    localStorage.setItem("backgroundColor", boxElTwo.classList.toggle("move-style") )
+})
